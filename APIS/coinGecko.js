7f7f5beda89808876b6be99edcbf58e6c9ff682c
CatBoxy <thecatbox@protonmail.com>
2021-09-28 10:45:00 -0300

    update package.json

--- APIS/coinGecko.js
+++ APIS/coinGecko.js
@@ -3,17 +3,17 @@ const CoinGecko = require('coingecko-api');
 const CoinGeckoClient = new CoinGecko();
 
 const coinGecko = {
-    // Consulta el precio de una cripto en la API de coingecko
+    // Queries the price of a criptocurrency to the coingecko API
     criptoPrice: async(cripto) => {
 
         let criptoToLowerCase = cripto.toString().toLowerCase();
 
-    // realiza la consulta a la API
+    // Queries the API
         let geckoJson = await CoinGeckoClient.simple.price({
             ids: criptoToLowerCase,
             vs_currencies: 'usd',
         });
-    // Almacena el dato del objeto recibido por la API, USD, en priceUsd
+    // Stores the data value recieved from the API 
         const priceValue = Object.values(geckoJson.data)[0];
         const priceUsd = priceValue.usd;
 
